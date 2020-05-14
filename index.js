ea52372faf3994034730b1b3650bb197201b9da8
FreshF <felix.pohlmann@gmx.de>
2020-05-14 15:33:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var io = require('socket.io')(process.env.PORT || 52300);
 io.on('connection', function(socket) {
     console.log('Game has connected!');
 
+    socket.on('hello', function() {
+        console.log('Yo wazuuup!')
+    })
+
 
 
     socket.on('disconnect', function() {
