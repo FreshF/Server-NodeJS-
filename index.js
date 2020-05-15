830e3a7656001f7383065b8ad19a8daf39c8defe
lidewij <lidewij122@gmail.com>
2020-05-15 16:22:51 +0200

    test with express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
+    io = require('socket.io').listen(server);
 
     server.listen(process.env.PORT || 52300);
 
