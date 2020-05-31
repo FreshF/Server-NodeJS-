5a4a54db8b1c15594118984a4c4cc9c8bb054274
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2020-05-31 18:16:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,83 +1,27 @@
-var io = require('socket.io')(process.env.PORT || 52300);
+//port
+var websocketPort = 52300;
 
-let vr = null;
-let app = null;
+// start websockets
+var io = require('socket.io').listen(websocketPort);
+console.log('Server is running on port ' + websocketPort);
 
-let gameRegistered = false;
+//websocket communication
+io.sockets.on('connection', function(socket){
+    // tell all clients who has connected
+    var ipAddress = socket.handshake.address;
+    io.sockets.emit('message', {name: ipAddress, text: 'joined'});
+    console.log('New connection from ' + ipAddress);
 
-io.on('connection', function(socket) {
-    console.log('Connected' + socket);
+    // relay message of client to all other clients
+    socket.on('message', function(data){
+        console.log('Received message ');
+        console.log('New message: ' + data);
+        io.sockets.emit('message', {name: data.name, text: data.text});
+    });
 
-    if (gameRegistered == false) {
-        gameRegistered = true;
-        socket.broadcast.emit('start', {id: "1"});
-    }
-    
-
-    //Send data from server to the client
-    //socket.emit('register', {id: "1"});
-    //socket.emit('start');
-
-    socket.on('register', (message) => { // when register event received
-        console.log("got register");
-        switch (message) { //check if its from the vr of app
-            case 'vr': //when its from the vr
-                registerVrEvents(socket); //register vr handlers
-                gameRegistered = true;
-
-
-
-                //socket.emit('sendData', "data");
-                break;
-            case 'app'://when its from app
-                registerAppEvents(socket); //register app handlers
-                break;
-        }
+    // tell all clients who has disconnected
+    socket.on('disconnect', function(){
+        io.sockets.emit('message', {name: ipAddress, text: 'left'});
+        console.log('client [' + ipAddress + '] disconnected');
     });
 });
-
-function registerAppEvents(socket) {
-    app = socket; //set the socket to the app variable
-    app.emit('event', 'app registered'); //send message that its registered
-
-
-    app.on('event', message => { //when event received from app
-        console.log("From app: " + message);
-        if(vr) { //if vr connected
-            vr.emit('event', message); //pass message from app to the vr
-        }
-    });
-
-
-
-
-    app.on('sendData', data => { //when event received from app
-        console.log("sending data");
-        //if(vr) { //if vr connected
-            vr.emit('sendData', data); //pass message from app to the vr
-        //}
-    });
-
-    socket.on('disconnect', function() { //when app disconnects
-        console.log("App Disconnected");
-        app = null; //set app variable to null again
-    });
-}
-
-function registerVrEvents(socket) {
-    vr = socket; //set the socket to the vr variable
-    vr.emit('event', 'vr registered');//send message that its registered
-    vr.on('event', message => {//when event received from vr
-        console.log("From VR: " + message);
-        vr.emit('sendData', "data");
-
-        if(app) { //if app connected
-            app.emit('event', message);//pass message from vr to the app
-        }
-    });
-
-    /* socket.on('disconnect', function() { //when vr disconnects
-        console.log("VR Disconnected");
-        vr = null; //sets vr variable to null again
-    }); */
-}
