e19c2b946d85022469f5d6c706879bf002ba0d89
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2020-05-31 18:41:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,22 +12,27 @@ console.log('--- Server has started on port: '+websocketPort+' ---');
 io.on('connection', function(socket){
     console.log('New connection');
 
-
     // tell all clients who has connected
     var ipAddress = socket.handshake.address;
-    io.sockets.emit('message', {name: ipAddress, text: 'joined'});
-    console.log('New connection from ' + ipAddress);
+    io.emit('connect', {key: ipAddress, value: 'connected'});
 
-    // relay message of client to all other clients
+    //react to connect event
+    socket.on('connect', function(data){
+        console.log(data.key + ' ' + data.value);
+        //io.emit('message', {name: data.name, text: data.text});
+    });
+
+    /* // relay message of client to all other clients
     socket.on('message', function(data){
         console.log('Received message ');
         console.log('New message: ' + data);
         io.sockets.emit('message', {name: data.name, text: data.text});
-    });
+    }); */
 
     // tell all clients who has disconnected
     socket.on('disconnect', function(){
-        io.sockets.emit('message', {name: ipAddress, text: 'left'});
-        console.log('client [' + ipAddress + '] disconnected');
+        //io.emit('message', {name: ipAddress, text: 'left'});
+        //console.log('client [' + ipAddress + '] disconnected');
+        console.log("Disconnected");
     });
 });
